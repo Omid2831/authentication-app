2d26aa3bdf6c587e9f80748a814dd0c8e71103c4
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-27 17:24:26 +0100

    feat: add RoleManagement controller and React component for role management functionality

---
+++ resources/js/Pages/features/RollManagement/RoleManagment.jsx
@@ -0,0 +1,14 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
+import React from 'react'
+
+
+const RoleManagment = (props) => {
+  return (
+    <AuthenticatedLayout
+      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">{props.title} Dashboard</h2>}
+    >
+    </AuthenticatedLayout>
+  )
+}
+
+export default RoleManagment
